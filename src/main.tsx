ef119780f096ef63a593a466f4f699ef14f90584
NightfuryEquinn <xianzyip@gmail.com>
2026-07-11 22:46:08 +0800

    chore: :sparkles: Vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
@@ -6,6 +7,7 @@ const elem = document.getElementById("root")!;
 const app = (
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
 
